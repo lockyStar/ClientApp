58dab7f1a1c3a1790f984d0ca1e485a2e1e60f0d
LockyStar <Zhabrevalexander@yandex.ru>
2016-11-13 03:11:07 +0300

    4 buttons added

---
+++ app/src/main/java/com/cars/client/GameActivity.java
@@ -0,0 +1,14 @@
+package com.cars.client;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class GameActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+
+}
